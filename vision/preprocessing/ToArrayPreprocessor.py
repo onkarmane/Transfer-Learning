72bf782d53c12a8977c7ba25f9280289ee566f07
onkarmane <onkar373@gmail.com>
2022-06-29 18:26:20 +0530

    Add files via upload

---
+++ vision/preprocessing/ToArrayPreprocessor.py
@@ -0,0 +1,25 @@
+
+'''
+'''
+
+
+from keras.preprocessing.image import img_to_array
+
+
+class ToArrayPreprocessor:
+    '''
+    '''
+    def __init__(self, dataFormat = None):
+        '''
+        Store image data format
+        '''
+        self.dataFormat = dataFormat
+
+
+    def preprocess(self, image):
+        # apply the keras utility function that correctly
+        # rearranges the dimensions of the image
+        return img_to_array(image, data_format = self.dataFormat)
+
+
+    
---
+++ vision/preprocessing/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+from .SimplePreprocessor import SimplePreprocessor
+from .ToArrayPreprocessor import ToArrayPreprocessor
+from .AspectPreprocessor import AspectPreprocessor
+
